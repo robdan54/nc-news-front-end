ded09e9188c4562b3582698352f5d5f21f444b82
Robin Cade <robin.cade54@gmail.com>
2022-03-09 11:26:45 +0000

    added function to filter articles

--- src/utils/articlesApi.js
+++ src/utils/articlesApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const fetchArticles = () => {
-    return axios.get('https://nc-news-robin.herokuapp.com/api/articles').then((res) => {
+export const fetchArticles = (topic) => {
+    return axios.get('https://nc-news-robin.herokuapp.com/api/articles', {params : {topic}}).then((res) => {
         return res.data.articles
     })
 }
