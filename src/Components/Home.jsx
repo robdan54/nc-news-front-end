03de18de85024e3f5ca2f85810739245746dc0df
Robin Cade <robin.cade54@gmail.com>
2022-03-11 14:48:13 +0000

    started sortby

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,13 +3,16 @@
 import { ListGroup, DropdownButton, Dropdown } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 
+
 import * as articlesApi from '../utils/articlesApi';
 
 import ArticleCard from './ArticleCard';
 import { fetchTopics } from '../utils/topicsApi';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
 	const [topics, setTopics] = useState([]);
+	const [sortBy, setSortBy] = useState(undefined);
 	const [filter, setFilter] = useState(undefined);
 	const [isLoading, setIsLoading] = useState(true);
 	const [articles, setArticles] = useState([]);
@@ -17,10 +20,10 @@ export default function Home() {
 	useEffect(() => {
 		setIsLoading(true);
 		fetchTopics().then((topicsFromApi) => {
-			setTopics(topicsFromApi.map((topicFromApi) => topicFromApi.slug))
-			setIsLoading(false)
-		})
-	},[]);
+			setTopics(topicsFromApi.map((topicFromApi) => topicFromApi.slug));
+			setIsLoading(false);
+		});
+	}, []);
 
 	useEffect(() => {
 		setIsLoading(true);
@@ -32,11 +35,38 @@ export default function Home() {
 
 	return !isLoading ? (
 		<div className='mb-2 articleList'>
-			<DropdownButton id='filter' title={filter || 'Filter' } variant='dark' style={{alignSelf:'start'}}>
+			<DropdownButton
+				id='filter'
+				title={filter || 'Filter'}
+				variant='dark'
+				style={{ alignSelf: 'start' }}>
 				{topics.map((topic) => {
-					return <Dropdown.Item key={topic} onClick={() => setFilter(topic)}>{ topic }</Dropdown.Item>;
+					return (
+						<Dropdown.Item key={topic} onClick={() => setFilter(topic)}>
+							{topic}
+						</Dropdown.Item>
+					);
 				})}
-				<Dropdown.Item key='reset' onClick={() => setFilter(undefined)}>Reset</Dropdown.Item>
+				<Dropdown.Item key='reset' onClick={() => setFilter(undefined)}>
+					Reset
+				</Dropdown.Item>
+			</DropdownButton>
+			<DropdownButton
+				id='sortBy'
+				title={sortBy || 'Sort-by'}
+				variant='dark'
+				style={{ alignSelf: 'start' }}>
+				<Dropdown.Item
+					key='author'
+					onClick={() => {
+						setSortBy('author');
+						navigate('/?sort_by=author');
+					}}>
+					Author
+				</Dropdown.Item>
+				<Dropdown.Item key='reset' onClick={() => setSortBy(undefined)}>
+					Reset
+				</Dropdown.Item>
 			</DropdownButton>
 			<ListGroup variant='flush'>
 				{articles.map((article) => {
